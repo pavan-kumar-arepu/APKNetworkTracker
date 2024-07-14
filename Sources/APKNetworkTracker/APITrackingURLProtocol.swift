0290e35bda9aff23c2bddf53ce2305e93ba1610d
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-14 17:49:31 +0200

    Updated again on the same

--- Sources/APKNetworkTracker/APITrackingURLProtocol.swift
+++ Sources/APKNetworkTracker/APITrackingURLProtocol.swift
@@ -49,7 +49,8 @@ class APITrackingURLProtocol: URLProtocol {
         task.resume()
     }
 
-    */
+
+    
     override func startLoading() {
         let task = URLSession.shared.dataTask(with: request) { [weak self] ( data, response, error) in
             guard let self = self else { return }
@@ -67,7 +68,24 @@ class APITrackingURLProtocol: URLProtocol {
         }
         task.resume()
     }
+*/
 
+    override func startLoading() {
+        let newRequest = (self.request as NSURLRequest).mutableCopy() as! NSMutableURLRequest
+        URLProtocol.setProperty(true, forKey: "MyURLProtocolHandledKey", in: newRequest)
+        URLSession.shared.dataTask(with: newRequest as URLRequest) { data, response, error in
+            if let error = error {
+                self.client?.urlProtocol(self, didFailWithError: error)
+            } else if let response = response, let data = data {
+                self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+                self.client?.urlProtocol(self, didLoad: data)
+                APITracker.trackAPICall(request: self.request, response: response as? HTTPURLResponse, responseData: data as Data?)
+                self.client?.urlProtocolDidFinishLoading(self)
+            }
+        }.resume()
+    }
+
+    
     /*
     override func startLoading() {
       guard let client = self.client else { return }
