7e0ecc19d776dc3e45c7c4f62393ab9bb0bdfa32
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-14 17:32:52 +0200

    Updated URLProcotol class

--- Sources/APKNetworkTracker/APITrackingURLProtocol.swift
+++ Sources/APKNetworkTracker/APITrackingURLProtocol.swift
@@ -18,20 +18,56 @@ class APITrackingURLProtocol: URLProtocol {
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
-   
+//
+//
+//    override func startLoading() {
+//        // Implement logic to track API request/response
+//        guard let client = self.client else { return }
+//
+//        // Continue loading original request
+//        client.urlProtocolDidFinishLoading(self)
+//
+//        // Example: track request
+//        APITracker.trackAPICall(request: request, response: nil, responseData: nil)
+//    }
 
+    /*
     override func startLoading() {
-        // Implement logic to track API request/response
-        guard let client = self.client else { return }
-
-        // Continue loading original request
-        client.urlProtocolDidFinishLoading(self)
-        
-        // Example: track request
-        APITracker.trackAPICall(request: request, response: nil, responseData: nil)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            if let error = error {
+                self.client?.urlProtocol(self, didFailWithError: error)
+                } else {
+                    if let data = data, let response = response {
+                        self.client?.urlProtocol(self, didLoad: data)
+                        self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+                        APITracker.trackAPICall(request: self.request, response: response as? HTTPURLResponse, responseData: data)
+                    }
+                    self.client?.urlProtocolDidFinishLoading(self)
+                }
+        }
+        task.resume()
     }
 
-    
+    */
+    override func startLoading() {
+        let task = URLSession.shared.dataTask(with: request) { [weak self] ( data, response, error) in
+            guard let self = self else { return }
+
+            if let error = error {
+                self.client?.urlProtocol(self, didFailWithError: error)
+            } else {
+                if let data = data, let response = response {
+                    self.client?.urlProtocol(self, didLoad: data)
+                    self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+                    APITracker.trackAPICall(request: self.request, response: response as? HTTPURLResponse, responseData: data)
+                }
+                self.client?.urlProtocolDidFinishLoading(self)
+            }
+        }
+        task.resume()
+    }
+
     /*
     override func startLoading() {
       guard let client = self.client else { return }
